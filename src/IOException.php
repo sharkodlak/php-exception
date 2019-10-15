f5ca98903e61d72e552de3e8d7f2f3437006f079
Pavel Štětina <github@shark.kom.cz>
2019-10-15 08:14:51 +0200

    Fixes missing IOException

---
+++ src/IOException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sharkodlak\Exception;
+
+/** Signals that an I/O exception of some sort has occurred.
+ *
+ * This class is the general class of exceptions produced by failed
+ * or interrupted I/O operations.
+ */
+class IOException extends Exception {
+}
